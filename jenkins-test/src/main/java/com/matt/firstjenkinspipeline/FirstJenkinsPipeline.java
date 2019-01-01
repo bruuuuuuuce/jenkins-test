06ac89e171c29bc32290becd18485d8d55163790
matt <matt.straat@gmail.com>
2018-12-31 16:36:47 -0800

    First commit

---
+++ jenkins-test/src/main/java/com/matt/firstjenkinspipeline/FirstJenkinsPipeline.java
@@ -0,0 +1,17 @@
+package com.matt.firstjenkinspipeline;
+
+public class FirstJenkinsPipeline {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int val = testMethod();
+		
+		System.out.println("Hello this is a test");
+	}
+	
+	public static int testMethod() {
+		return 8;
+	}
+
+}
---
+++ jenkins-test/src/main/test/com/test/firstjenkinspipeline/FirstJenkinsPipelineTest.java
@@ -0,0 +1,12 @@
+package com.test.firstjenkinspipeline;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstJenkinsPipelineTest {
+
+	@Test
+	public void test1() {
+		Assert.assertTrue(true);
+	}
+}
